b2dc0e8354863d024956fcd04caf80a635812370
JJCatolico <heavyweightj@gmail.com>
2017-08-16 17:21:03 -0400

    second push

--- app.rb
+++ app.rb
@@ -6,6 +6,17 @@ set :database, { adapter: "sqlite3", database: "development.sqlite3" }
 
 enable :sessions
 
+
+
+before do
+	@current_user ||= User.find_by_id(session[:user_id])
+end
+
+def logged_in?
+ @current_user && @current_user.id == session[:user_id]
+ end
+
+
 get '/' do 
 	erb :index
 end
@@ -20,10 +31,13 @@ get '/login' do
 end
 
 post '/login' do 
-	user = User.find_by()
+	user = User.find_by(username: params[:user][:username])
+	if user && user.password == params[:user][:password]
 	session[:user_id] = user.id
 	redirect '/'
-
+else 
+	redirect '/login'
+end
 end
 
 get '/signup' do
