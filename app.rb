32709841b090608bdfe00516ffb6372a38144ac3
JJCatolico <heavyweightj@gmail.com>
2017-08-17 15:56:28 -0400

    pushing new content

--- app.rb
+++ app.rb
@@ -8,17 +8,17 @@ enable :sessions
 
 
 
-before do
-	@current_user ||= User.find_by_id(session[:user_id])
+def current_user
+@current_user ||= User.find_by_id(session[:user_id])
 end
 
 def logged_in?
  @current_user && @current_user.id == session[:user_id]
- end
+end
 
 
-get '/' do 
-	erb :index
+get '/' do
+  erb :index, :layout => :post
 end
 
 get '/users/dashboard' do 
@@ -33,12 +33,13 @@ end
 post '/login' do 
 	user = User.find_by(username: params[:user][:username])
 	if user && user.password == params[:user][:password]
-	session[:user_id] = user.id
-	redirect '/'
-else 
-	redirect '/login'
+		session[:user_id] = user.id
+		redirect '/'
+	else 
+		redirect '/login'
+	end
 end
-end
+
 
 get '/signup' do
 	erb :signup
@@ -54,4 +55,13 @@ get '/logout' do
 	session[:user_id] = nil
 	redirect '/'
 end
+
+get '/posts' do
+	erb:posts
+end
+
+post '/posts' do 
+  "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+end
+
 
